7a2f94b74987fc3163d329aff598b4fa58eba377
suyash10092005 <102400938+suyash10092005@users.noreply.github.com>
2022-09-01 13:21:46 +0530

    Add files via upload

---
+++ Site1/scripts/form-carousel_4c83.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'New Form Submission',
+    'email_message' => 'You have a new form submission',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => 'suyashmaskara2005@gmail.com',
+    'to' => 'suyashmaskara2005@gmail.com'
+    ),
+    'fields' => array(
+    'name' => array(
+    'order' => 1,
+    'type' => 'string',
+    'label' => 'Name',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Name\' is required.'
+    )
+    ),
+    'mobilenumber' => array(
+    'order' => 2,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor('');
+    $processor->process($form);
+
+    ?>
